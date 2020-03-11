cfc31ffb7c2fd2b9f13995412dd1c255a7332bda
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2020-03-11 14:50:20 +0700

    ProductSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        // koding ini digunakan untuk menjalakan seeder
+        $this->call(ProductTableSeeder::class);
     }
 }
