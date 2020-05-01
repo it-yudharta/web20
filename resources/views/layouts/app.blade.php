746471cbd438a4eece3146d4cdc237b23ee28a0e
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2020-05-01 10:48:46 +0700

    middleware auth

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,12 +33,14 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
+                        @auth
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('home') }}">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('products.index') }}">Barang</a>
                         </li>
+                        @endauth
                     </ul>
 
                     <!-- Right Side Of Navbar -->
